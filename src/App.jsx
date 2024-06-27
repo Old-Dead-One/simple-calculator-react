de30e1f3232ad1625cee4043f0505245c9d45052
Alan McGraw <alanmcgraw@proton.me>
2024-06-27 11:23:07 -0600

    Add componants and logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
 import "./App.css";
+import Calculator from "./Calculator";
 
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <header>
+        <h1>Calculator</h1>
+      </header>
+      <Calculator />
+      <footer>
+        <p>Created by Old Dead One</p>
+      </footer>
+    </>
+  );
 }
